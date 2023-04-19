4ffb62370e8949c1cc5e3c0efd0cfbe5491741cb
gabrielmop <gabriel_moop@hotmail.com>
2023-04-19 11:55:30 -0300

    Adição da escrita de nome e lançamento e otimização de codigo

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using System.Windows.Forms;
 
 namespace GameCardCreator
 {
-    internal static class Program
+    internal static class Program 
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -10,25 +10,48 @@ namespace GameCardCreator
         [STAThread]
         static void Main()
         {
+            string nome, lancamento, genero, dev, publi, d1, d2, d3, d4, d5;
+            PictureBox pictureBox1 = new PictureBox();
+            int tamanho = 20;
+            Font fonte = new("zrnic rg", tamanho);
+            SolidBrush corTexto = new SolidBrush(Color.White);
+          
+
+
             Console.WriteLine("Bem vindo ao GameCardCreator");
             Console.WriteLine("Esse software serve para Criar cards de jogos de PS1 para o PS vita");
-            Console.WriteLine("Por favor escolha a imagem que será usada para o Card:");
-            PictureBox pictureBox1 = new PictureBox();
+            Console.WriteLine("Por favor, escolha a imagem que será usada para o Card:");
 
-            // Carrega a imagem em um controle PictureBox
-            pictureBox1.Image = Image.FromFile("imagem.jpg");
+            OpenFileDialog arquivo = new OpenFileDialog();
+            arquivo.Filter = "Arquivos de imagem (*.jpg)(*.png)|*.jpg";
+            arquivo.Title = "Escolha um arquivo de imagem";
 
-            // Cria um objeto Graphics para desenhar na imagem
+            if (arquivo.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(arquivo.FileName);
+            }
+
             Graphics g = Graphics.FromImage(pictureBox1.Image);
 
-            // Define as propriedades do objeto Graphics
-            Font fonte = new Font("Arial", 20);
-            SolidBrush corTexto = new SolidBrush(Color.White);
+            Console.WriteLine("Escreva o nome do Jogo");
+            nome = Console.ReadLine();
 
-            // Desenha o texto na imagem
-            g.DrawString("Texto de exemplo", fonte, corTexto, new PointF(10, 10));
+            if (nome.Length >= 27)
+            {
+                tamanho = 14;
+            }
+           
+            g.DrawString(nome, fonte, corTexto, new PointF(560, 71));
 
-            // Salva a imagem atualizada em um arquivo
+            Console.WriteLine("Escreva a Data de Lançamento do Jogo");
+            lancamento = $"Lançamento:{Environment.NewLine} {Console.ReadLine()}";
+            tamanho = 20;
+
+            g.DrawString(lancamento, fonte, corTexto, new PointF(555, 110));
+
+
+
+
             pictureBox1.Image.Save("imagem_com_texto.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
 
             
